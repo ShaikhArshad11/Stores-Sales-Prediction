1329e33cb8e057f1919edc626907bdde54ffa011
Arshad Shaikh <shaikh121arshad@gmail.com>
2023-04-02 12:42:17 +0530

    Updated

--- app.py
+++ app.py
@@ -2,60 +2,75 @@ from flask import Flask , request, render_template, jsonify
 import pickle
 import numpy as np
 from mongodb import db_ops
+from src.logger import logging
+from src.exception import CustomException
+import sys
 
 app =Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    logging.info("Initiated 'index.html'")
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        raise CustomException(e,sys) from e
 
 @app.route('/predict', methods = ['GET','POST'])
 def predict():
-    
-    Item_Weight= float(request.form['Item_Weight'])
-    Item_Fat_Content=int(request.form['Item_Fat_Content'])
-    Item_Visibility= float(request.form['Item_Visibility'])
-    Item_Type= int(request.form['Item_Type'])
-    Item_MRP= float(request.form['Item_MRP'])
-    Outlet_Location_Type= int(request.form['Outlet_Location_Type'])
-    Outlet_Size= int(request.form['Outlet_Size'])
-    Outlet_Type_Supermarket_Type1= int(request.form['Outlet_Type_Supermarket_Type1'])
-    Outlet_Type_Supermarket_Type2= int(request.form['Outlet_Type_Supermarket_Type2'])
-    Outlet_Type_Supermarket_Type3= int(request.form['Outlet_Type_Supermarket_Type3'])
+    logging.info("Started '/predict' api")
+    try:
+        Item_Weight= float(request.form['Item_Weight'])
+        Item_Fat_Content=int(request.form['Item_Fat_Content'])
+        Item_Visibility= float(request.form['Item_Visibility'])
+        Item_Type= int(request.form['Item_Type'])
+        Item_MRP= float(request.form['Item_MRP'])
+        Outlet_Location_Type= int(request.form['Outlet_Location_Type'])
+        Outlet_Size= int(request.form['Outlet_Size'])
+        Outlet_Type_Supermarket_Type1= int(request.form['Outlet_Type_Supermarket_Type1'])
+        Outlet_Type_Supermarket_Type2= int(request.form['Outlet_Type_Supermarket_Type2'])
+        Outlet_Type_Supermarket_Type3= int(request.form['Outlet_Type_Supermarket_Type3'])
 
-    x = np.array([[ Item_Weight,Item_Fat_Content,Item_Visibility,Item_Type,Item_MRP,
-     Outlet_Location_Type,Outlet_Size,Outlet_Type_Supermarket_Type1,
+        x = np.array([[ Item_Weight,Item_Fat_Content,Item_Visibility,Item_Type,Item_MRP,
+        Outlet_Location_Type,Outlet_Size,Outlet_Type_Supermarket_Type1,
         Outlet_Type_Supermarket_Type2,Outlet_Type_Supermarket_Type3 ]])
     
-    prediction = model.predict(x)
-    output = prediction**3
-    output = round(output[0], 2)
-    #return jsonify({'Prediction': float(prediction)})
+        prediction = model.predict(x)
+        output = prediction**3
+        output = round(output[0], 2)
+        #return jsonify({'Prediction': float(prediction)})
 
-    return render_template('index.html', prediction_text='Predicted Sales $ {}'.format(output))
+        return render_template('index.html', prediction_text='Predicted Sales $ {}'.format(output))
+    
+    except Exception as e:
+            raise CustomException(e,sys) from e
 
 @app.route('/predict_all', methods=['POST'])
-
 def predict_all():
-    data = [x for x in request.form.values()]
-    #[np.array(data)]
-    database=''.join(data[-2:-1])
-    collection=''.join(data[-1:])
-    print(database,collection)
+    logging.info("Started '/predict_all' api")
+    try:
+        data = [x for x in request.form.values()]
+        #[np.array(data)]
+        database=''.join(data[-2:-1])
+        collection=''.join(data[-1:])
+        print(database,collection)
 
-    db_obj = db_ops(database, collection)
-    df = db_obj.load_df()
-    print(df)
-    if type(df) == str:
-        return render_template('index.html', Error=df)
-    else:
-        #Test transformation
-        X=df[[x for x in df.columns if x != 'Classes']]
-        print(X)
-        my_prediction=model.predict(X.values)
-        my_prediction=my_prediction.tolist()
-        return render_template('home.html',prediction = my_prediction)
+        db_obj = db_ops(database, collection)
+        df = db_obj.load_df()
+        print(df)
+        if type(df) == str:
+            return render_template('index.html', Error=df)
+        else:
+            #Test transformation
+            X=df[[x for x in df.columns if x != 'Classes']]
+            print(X)
+            my_prediction=model.predict(X.values)**3
+            my_prediction=my_prediction.tolist()
+            return render_template('index.html',prediction = my_prediction)
+        
+    except Exception as e:
+            raise CustomException(e,sys) from e
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME='sales-predictor'
+AUTHOR='Arshad Shaikh'
+VERSION='0.0.1'
+DESCRIPTION='Stores Sales Prediction'
+PACKAGES=['src']
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+def get_requirement_list()->List[str]:
+     """"
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+     with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove('-e .')
+
+setup(name=PROJECT_NAME,
+      version=VERSION,
+      author=AUTHOR,
+      description=DESCRIPTION,
+      packages=PACKAGES,
+      install_requires=get_requirement_list()
+      )
